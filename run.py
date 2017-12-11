0caa671f6ec9f71fb18d16d9957b7d00d75f7aa3
revannth.github.io <revannth1997@gmail.com>
2017-12-12 00:05:13 +0530

    MVC idea trashed

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
 #run.py
 
-from app import app
+from flask import Flask,render_template,request
+from pusher import Pusher
+import json
+
+app = Flask(__name__)
+
+pusher= Pusher{
+	app_id = "442574"
+	key = "dc8b02b3828c119a75ea"
+	secret = "39834eadbe283ebc4f69"
+	cluster = "ap2"
+}
+
+
+
+
+
+
+
+
+
 app.run(debug=True)
